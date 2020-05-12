8ffd539a788d119a48f1fb9d555c3bff1cb0e92d
henriqueten <35928909+henriqueten@users.noreply.github.com>
2020-05-11 22:14:48 -0300

    Updates

---
+++ precache-manifest.f6dacc270327111e227075dea09892ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc3db4f844c347af04d9aa34973e759",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "336f66305189124e8b7f",
+    "url": "/production/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "83e7dad03bfe72bf19fc",
+    "url": "/production/static/js/2.2250775b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/production/static/js/2.2250775b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336f66305189124e8b7f",
+    "url": "/production/static/js/main.cdf1a2f5.chunk.js"
+  },
+  {
+    "revision": "24a0f8eac704ca290e99",
+    "url": "/production/static/js/runtime-main.239b538a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/production/static/media/logo.5d5d9eef.svg"
+  }
+]);
